5a6aab23354581eb5c92a04aa92713b13330bfad
DoubleS-Lee <lss8825@gmail.com>
2020-06-05 21:48:53 +0900

    200605_댓글 일부 완성

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,4 +4,4 @@ from . import views
 
 app_name = "reviews"
 
-urlpatterns = [path("r_create/<int:content>/", views.CreateReviewView.as_view(), name="create_review")]
+urlpatterns = [path("r_create/<int:content>/", views.CreateReviewView, name="create_review")]
--- reviews/views.py
+++ reviews/views.py
@@ -4,29 +4,13 @@ from contents import models as content_models
 from . import forms, models
 from django.views.generic import FormView
 
-
-class CreateReviewView(FormView):
-
-    template_name = "contents/content_detail.html"
-    form_class = forms.ReviewForm
-
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
-
-'''
-def create_review(request, content):
-    post = get_object_or_404(content_models.Content, pk=content)
-    if request.method == "POST":
-        form = forms.ReviewForm(request.POST)
-        if form.is_valid():
-            review = form.save(commit=False)
-            review.user = request.users
-            review.content = content
-            review.save()
-            return redirect(reverse("contents:detail", post.pk))
-    
-    else:
-        form = forms.ReviewForm()
-    return render(request, 'contents/content_detail.html', {'form':form},)
-'''
+def CreateReviewView(request, content, *args, **kwargs):
+        if request.method == 'POST':
+            comment = models.Review()
+            comment.review = request.POST['review']
+            comment.content = content_models.Content.objects.get(pk=content) # id로 객체 가져오기        
+            comment.user = request.user
+            comment.save()
+            return redirect(reverse("contents:detail", kwargs={"pk": content}))
+        else :
+            return redirect('core:home')
