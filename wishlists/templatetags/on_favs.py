5f5960fb6444bc90a70e765c4851805605043a31
DoubleS-Lee <lss8825@gmail.com>
2020-06-17 21:35:29 +0900

    add createsu.py

--- wishlists/templatetags/on_favs.py
+++ wishlists/templatetags/on_favs.py
@@ -7,8 +7,11 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def on_favs(context, content):
-    user = context.request.user
-    the_list = wishlist_models.Wishlist.objects.get_or_none(
-        user=user, name="My Favourites Houses"
-    )
-    return content in the_list.contents.all()
+    if contents:
+        user = context.request.user
+        the_list = wishlist_models.Wishlist.objects.get_or_none(
+            user=user, name="My Favourites Houses"
+        )
+        return content in the_list.contents.all()
+    else:
+        pass
