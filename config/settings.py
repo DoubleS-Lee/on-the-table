8ec44f2db97cc93b02968ecdddc5c343c057eb9c
DoubleS-Lee <lss8825@gmail.com>
2020-06-17 23:51:09 +0900

    collectstatic

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,7 @@ DJANGO_APPS = [
     'django.contrib.staticfiles',
 ]
 
-THIRD_PARTY_APPS = ["django_countries", "django_seed", "taggit.apps.TaggitAppConfig", "taggit_templatetags2",]
+THIRD_PARTY_APPS = ["django_countries", "django_seed", "taggit.apps.TaggitAppConfig", "taggit_templatetags2", "storages"]
 
 PROJECT_APPS = [
     "core.apps.CoreConfig", 
@@ -168,7 +168,22 @@ TAGGIT_CASE_INSENSITIVE = True
 TAGGIT_LIMIT = 50
 
 # Sentry
+
+
+
 if not DEBUG:
+
+    DEFAULT_FILE_STORAGE = "config.custom_storages.UploadStorage"
+    STATICFILES_STORAGE = "config.custom_storages.StaticStorage"
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = "on-the-table-ont"
+    AWS_AUTO_CREATE_BUCKET = True
+    AWS_BUCKET_ACL = "public-read"
+
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+
     sentry_sdk.init(
         dsn=os.environ.get("SENTRY_URL"),
         integrations=[DjangoIntegration()],
